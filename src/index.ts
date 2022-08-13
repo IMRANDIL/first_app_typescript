f8d140a1b607358990b89b4cf8efb694062390d2
IMRANDIL <aliimranadil2@gmail.com>
2022-08-13 09:55:56 +0530

    yh

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
-import { User } from "./User";
-import { Company } from "./Company";
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const user = new User();
-const company = new Company();
-
-
-console.log(company);
+// const user = new User();
+// const company = new Company();
 
 
-console.log(user);
+// console.log(company);
+
+
+// console.log(user);
+const element = document.getElementById('mapDiv') as HTMLElement;
+
+new google.maps.Map(element, {
+    zoom: 1,
+    center: {
+        lat: 10,
+        lng: 0
+    }
+
+})
 
